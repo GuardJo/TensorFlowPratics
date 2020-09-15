1189cc8762e5807e1b71081e7d0e2e5cf4ec722d
GuardJo <dagh1218@naver.com>
2020-09-15 15:14:50 +0900

    Chapter04 Complete

---
+++ test0.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+node1 = tf.constant(3.0)
+node2 = tf.constant(4.0)
+node3 = node1 + node2
+
+
+sess = tf.Session()
+print(sess.run([node1, node2]))
+print(sess.run(node3))
