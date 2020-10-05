4187d606a68c3c7f4917778ac7c853ac1f3e8741
GuardJo <dagh1218@naver.com>
2020-10-05 17:34:36 +0900

    jejoExample fix to training step

--- JejoTest2/jejoExample.py
+++ JejoTest2/jejoExample.py
@@ -10,13 +10,13 @@ y_data = xy[1:, [-1]]
 x = tf.placeholder(tf.float32, shape=[None, 4])
 y = tf.placeholder(tf.float32, shape=[None, 1])
 
-w = tf.Variable(tf.random_normal([4, 10]), name='weight')
+w = tf.Variable(tf.random_normal([4, 1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
 
 hypothesis = tf.matmul(x, w) + b
 
 cost = tf.reduce_mean(tf.square(hypothesis - y))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-2).minimize(cost)
 
 prediction = tf.equal(hypothesis, y)
 accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
@@ -25,15 +25,8 @@ accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    for step in range(10001):
+    for step in range(50001):
         cost_val, hy_val, y_val, _ = sess.run([cost, hypothesis, y, optimizer], feed_dict={x: x_data, y: y_data})
 
-        if step % 500 == 0:
+        if step % 5000 == 0:
             print("step : ", step, "cost : ", cost_val)
-
-    # p, c = sess.run([prediction, accuracy], feed_dict={x: x_data, y: y_data})
-    # print("prediction Test", p, "accuracy : ", c)
-
-    hy_val, y_val = sess.run([hypothesis, y], feed_dict={x: x_data, y: y_data})
-
-    print("hypothesis : ", hy_val, "y_value : ", y_val)
