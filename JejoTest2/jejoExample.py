2d7c523323ef31a57536daea6e3d4d78940dde50
GuardJo <dagh1218@naver.com>
2020-10-07 18:05:46 +0900

    Create testData3

--- JejoTest2/jejoExample.py
+++ JejoTest2/jejoExample.py
@@ -2,16 +2,16 @@ import numpy as np
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 
-arr = np.loadtxt("test2_2_1.csv", delimiter=',', dtype=np.object, encoding='utf-8')
+arr = np.loadtxt("test3.csv", delimiter=',', dtype=np.object, encoding='utf-8')
 
-x_data = arr[0:, 1: -1]
+x_data = arr[1:, 1: -1]
 x_data = MinMaxScaler().fit_transform(x_data)
-y_data = arr[0:, [-1]]
+y_data = arr[1:, [-1]]
 
-x = tf.placeholder(tf.float32, shape=[None, 4])
+x = tf.placeholder(tf.float32, shape=[None, 3])
 y = tf.placeholder(tf.float32, shape=[None, 1])
 
-w = tf.Variable(tf.random_normal([4, 1]), name='weight')
+w = tf.Variable(tf.random_normal([3, 1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
 
 hypothesis = tf.matmul(x, w) + b
@@ -26,10 +26,10 @@ accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    for step in range(200001):
+    for step in range(2001):
         cost_val, _ = sess.run([cost, optimizer], feed_dict={x: x_data, y: y_data})
 
-        if step % 1000 == 0:
+        if step % 100 == 0:
             print("step : ", step, "cost : ", cost_val)
 
     # p, a = sess.run([prediction, accuracy], feed_dict={x: x_data, y: y_data})
