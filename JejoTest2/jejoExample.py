0a7e347b44659165d4520862a67e23868ab0daaa
GuardJo <dagh1218@naver.com>
2020-10-08 18:01:23 +0900

    Create testData3_1

--- JejoTest2/jejoExample.py
+++ JejoTest2/jejoExample.py
@@ -2,11 +2,10 @@ import numpy as np
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 
-arr = np.loadtxt("test3.csv", delimiter=',', dtype=np.object, encoding='utf-8')
+arr = np.loadtxt("test3_1.csv", delimiter=',', dtype=np.object, encoding='utf-8')
 
-x_data = arr[1:, 1: -1]
-x_data = MinMaxScaler().fit_transform(x_data)
-y_data = arr[1:, [-1]]
+x_data = arr[0:, 1: -1]
+y_data = arr[0:, [-1]]
 
 x = tf.placeholder(tf.float32, shape=[None, 3])
 y = tf.placeholder(tf.float32, shape=[None, 1])
@@ -17,7 +16,7 @@ b = tf.Variable(tf.random_normal([1]), name='bias')
 hypothesis = tf.matmul(x, w) + b
 
 cost = tf.reduce_mean(tf.square(hypothesis - y))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-4).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-3).minimize(cost)
 
 prediction = tf.equal(hypothesis, y)
 accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
@@ -26,14 +25,14 @@ accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    for step in range(2001):
+    for step in range(500001):
         cost_val, _ = sess.run([cost, optimizer], feed_dict={x: x_data, y: y_data})
 
-        if step % 100 == 0:
+        if step % 1000 == 0:
             print("step : ", step, "cost : ", cost_val)
 
     # p, a = sess.run([prediction, accuracy], feed_dict={x: x_data, y: y_data})
     # print("Prediction : ", p, "Accuracy : ", a)
 
     hy_val, y_val = sess.run([hypothesis, y], feed_dict={x: x_data, y: y_data})
-    print("hypothesis : ", hy_val, "y : ", y_val)
+    print("hypothesis : \n", hy_val)
