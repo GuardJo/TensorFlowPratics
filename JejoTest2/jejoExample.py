76e91d57da50843756fdd0a8352e740afed98aa8
GuardJo <dagh1218@naver.com>
2020-10-12 17:56:56 +0900

    Change hypothesis

--- JejoTest2/jejoExample.py
+++ JejoTest2/jejoExample.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 
 arr = np.loadtxt("test3.csv", delimiter=',', dtype=np.object, encoding='utf-8')
-arr = MinMaxScaler().fit_transform(arr)
+# arr = MinMaxScaler().fit_transform(arr)
 arr2 = np.loadtxt("test3_2.csv", delimiter=',', dtype=np.object, encoding='utf-8')
-arr2 = MinMaxScaler().fit_transform(arr2)
+# arr2 = MinMaxScaler().fit_transform(arr2)
 
 x_data = arr[0:, 1: -1]
 
@@ -18,9 +18,10 @@ y_test = arr2[0:, [-1]]
 x = tf.placeholder(tf.float32, shape=[None, 3])
 y = tf.placeholder(tf.float32, shape=[None, 1])
 
-w = tf.Variable(tf.random_normal([3, 1]), name='weight')
+w = tf.Variable(tf.random_normal([1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
-hypothesis = tf.matmul(x, w) + b
+
+hypothesis = (((x[0] + x[2]) * w) / x[1]) * b
 
 cost = tf.reduce_mean(tf.square(hypothesis - y))
 optimizer = tf.train.AdamOptimizer(learning_rate=1e-2).minimize(cost)
@@ -33,11 +34,10 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     for epoch in range(1000) :
-        sess.run(optimizer, feed_dict={x: x_data, y: y_data})
+        hy_val, _ = sess.run([hypothesis, optimizer], feed_dict={x: x_data, y: y_data})
 
-        if (epoch + 1) % 50 == 0:
-            c = sess.run(cost, feed_dict={x: x_data, y: y_data})
-            print(("Epoch : ", '%04d' % (epoch + 1), "cost = ", "{:.9f}".format(c), "w = ", sess.run(w), "b = ",
-                   sess.run(b)))
+        if (epoch % 100 == 0):
+            print("hy_val : ", hy_val);
 
+    print("w_val : ", sess.run(w), "\nb_val : ", sess.run(b))
     print("Complete")
