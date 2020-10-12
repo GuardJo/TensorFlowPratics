effff3fa4bd0c1bd24505cdde097752cfe1e93d4
GuardJo <dagh1218@naver.com>
2020-10-12 17:25:27 +0900

    Change Epoch Count

--- JejoTest2/jejoExample.py
+++ JejoTest2/jejoExample.py
@@ -2,14 +2,14 @@ import numpy as np
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 
-arr = np.loadtxt("test3_1.csv", delimiter=',', dtype=np.object, encoding='utf-8')
+arr = np.loadtxt("test3.csv", delimiter=',', dtype=np.object, encoding='utf-8')
+arr = MinMaxScaler().fit_transform(arr)
 arr2 = np.loadtxt("test3_2.csv", delimiter=',', dtype=np.object, encoding='utf-8')
+arr2 = MinMaxScaler().fit_transform(arr2)
 
 x_data = arr[0:, 1: -1]
-x_data = MinMaxScaler().fit_transform(x_data)
 
 x_test = arr2[0:, 1:-1]
-x_test = MinMaxScaler().fit_transform(x_test)
 
 y_data = arr[0:, [-1]]
 
@@ -32,14 +32,12 @@ accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    for step in range(50001):
-        cost_val, _ = sess.run([cost, optimizer], feed_dict={x: x_data, y: y_data})
+    for epoch in range(1000) :
+        sess.run(optimizer, feed_dict={x: x_data, y: y_data})
 
-        if step % 1000 == 0:
-            print("step : ", step, "cost : ", cost_val)
+        if (epoch + 1) % 50 == 0:
+            c = sess.run(cost, feed_dict={x: x_data, y: y_data})
+            print(("Epoch : ", '%04d' % (epoch + 1), "cost = ", "{:.9f}".format(c), "w = ", sess.run(w), "b = ",
+                   sess.run(b)))
 
-    # p, a = sess.run([prediction, accuracy], feed_dict={x: x_data, y: y_data})
-    # print("Prediction : ", p, "Accuracy : ", a)
-
-    hy_val, w_val, b_val = sess.run([hypothesis, w, b], feed_dict={x: x_test, y: y_test})
-    print("hypothesis : \n", hy_val, "w_val : \n", w_val, "b_val : \n", b_val)
+    print("Complete")
